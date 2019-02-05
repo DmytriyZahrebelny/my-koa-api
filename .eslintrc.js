6f5fac8c5b16de776429e1d8365ade6a6a5cbd77
Dima <zahrebelny@gmail.com>
2019-02-06 01:57:43 +0200

    start

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    'browser': true
+  },
+  rules: {
+    'arrow-body-style': 0,
+    'linebreak-style': 0,
+    'no-plusplus': 'off',
+  },
+};
