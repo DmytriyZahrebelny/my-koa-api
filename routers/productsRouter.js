c25266afb2b0d37cbb8b8325ea481c00aaa14ad2
Dima <zahrebelny@gmail.com>
2019-02-17 01:51:43 +0200

    add delete products

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -4,17 +4,24 @@ const Products = require('../models/Products');
 
 const productsRouter = new Router();
 
-productsRouter.get('/products', async (ctx) => {
-  const users = await Products.find({});
-  ctx.body = users;
-}).post('/add', async (ctx) => {
-  await Products.create(Object.assign(ctx.request.body, { id: uuidv4() }));
+productsRouter
+  .get('/products', async (ctx) => {
+    ctx.body = await Products.find({});
+  })
+  .post('/add', async (ctx) => {
+    await Products.create(Object.assign(ctx.request.body, { id: uuidv4() }));
 
-  ctx.body = 'ok';
-}).del('/delete', async (ctx) => {
-  ctx.body = ctx.request.body;
+    ctx.body = await Products.find({});
+  })
+  .del('/delete/:id', async (ctx) => {
+    await Products.find({ id: ctx.params.id }).remove();
 
-  console.log(ctx.body)
-});
+    ctx.body = await Products.find({});
+  })
+  .patch('/change/:id', async (ctx) => {
+    console.log(ctx.params.id);
+
+    ctx.body = 'ok';
+  });
 
 module.exports = productsRouter;
