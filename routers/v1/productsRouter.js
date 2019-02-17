9f4c359fabde078884abb14fa9ceb76ef00bd71e
Dima <zahrebelny@gmail.com>
2019-02-18 01:07:18 +0200

    deploy v1

--- routers/v1/productsRouter.js
+++ routers/v1/productsRouter.js
@@ -1,24 +1,24 @@
 const Router = require('koa-router');
 const uuidv4 = require('uuid/v4');
-const Products = require('../models/Products');
+const Products = require('../../models/Products');
 
 const productsRouter = new Router();
 
 productsRouter
-  .get('/products', async (ctx) => {
+  .get('/api/v1/products', async (ctx) => {
     ctx.body = await Products.find({});
   })
-  .post('/add', async (ctx) => {
+  .post('/api/v1/add', async (ctx) => {
     await Products.create(Object.assign(ctx.request.body, { id: uuidv4() }));
 
     ctx.body = await Products.find({});
   })
-  .del('/delete/:id', async (ctx) => {
+  .del('/api/v1/delete/:id', async (ctx) => {
     await Products.find({ id: ctx.params.id }).remove();
 
     ctx.body = await Products.find({});
   })
-  .patch('/change/', async (ctx) => {
+  .patch('/api/v1/change/', async (ctx) => {
     const {
       title,
       type,
